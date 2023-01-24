327e03501db001c07e347cfc0ad6e496a7a7918b
AbderrahimAl <abderrahimalakouche@gmail.com>
2023-01-24 13:40:14 +0100

    ✏️Update tests & dependencies

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,16 +2,21 @@ from fastapi.testclient import TestClient
 from app.es_connection import client
 from app.api import app
 
+test_client = TestClient(app)
 
 def test_es_conn():
     """Test elasticsearch connection"""
 
     assert client.ping()
 
+def test_root():
+    
+    response = test_client.get("/")
+    assert response.status_code == 200
 
 def test_get_search():
     """Test search api"""
 
-    test_client = TestClient(app)
-    response = test_client.get("/?query=")
+    
+    response = test_client.get("/search?query=")
     assert response.status_code == 200
