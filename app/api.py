09fa8174827c513bda1fc8403a86b82b39ce2649
AbderrahimAl <abderrahimalakouche@gmail.com>
2023-01-24 13:33:08 +0100

    📝Create index page

--- app/api.py
+++ app/api.py
@@ -1,24 +1,39 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, Request, status
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from .es_connection import client
 
 
 app = FastAPI()
 
-@app.get("/", status_code=status.HTTP_200_OK)
+app.mount("/static", StaticFiles(directory="static"))
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("index.html", context)
+
+
+@app.get("/search", status_code=status.HTTP_200_OK)
 def search(query: str):
 
     search_params = {
-    "query": {
-        "match": {
-            "club_name": {
+        "query": {
+            "multi_match": {
                 "query": query,
-                "fuzziness": "AUTO",
-                "operator": "and"
+                "type": "bool_prefix",
+                "fields": [
+                    "club_name"
+                    ]
+                    }
                 }
-            }
-        }
-    }
-
-    results = client.search(index="clubs_index", **search_params)
-
-    return results
+             }
+        
+    response = client.search(index="clubs_index", **search_params)
+    a = [res["_source"]["club_name"] for res in response["hits"]["hits"]]
+    print(a)
+    return [res["_source"]["club_name"] for res in response["hits"]["hits"]]
